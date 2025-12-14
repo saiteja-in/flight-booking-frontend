2fedd0c6d76cf22c8e7e404f630902b76952c5dc
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-14 23:04:37 +0530

    added routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { Login } from './features/login/login';
+import { Signup } from './features/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'login',component:Login},
+  {path:'register',component:Signup},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
---
+++ src/app/features/signup/signup.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css',
+})
+export class Signup {
+
+}
